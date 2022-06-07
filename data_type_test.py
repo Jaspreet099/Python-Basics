72c540cf283174a6ccda7c960f031982077ab451
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-06-07 20:22:39 +0530

    Different Data types

---
+++ data_type_test.py
@@ -0,0 +1,7 @@
+# data type test 
+
+Name = "Jaspreet Singh Saini"
+Age =19;
+year_of_birth = 2002
+is_newUser = True;
+Fav_food = ["vada pav", "fries", "samosa"]
---
+++ data_types.py
@@ -0,0 +1,20 @@
+rating = 4.7
+print(rating, type(rating))
+
+name = "Jaspreet"
+print(name, type(name))
+
+marks = 98
+print(marks, type(marks))
+
+is_passed = True
+print(is_passed, type(is_passed))
+
+my_complex_number = 4+5j
+print(my_complex_number, type(my_complex_number))
+
+fruits_list = ['apple', 'banana', 'kiwi']
+print(fruits_list, type(fruits_list))
+
+
+
