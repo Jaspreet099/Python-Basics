e906d9e26490215d4f417a5a4a003144111be03e
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-06-07 20:11:23 +0530

    identifiers added

---
+++ identifer.py
@@ -0,0 +1,15 @@
+#valid identifiers
+age = 19;
+name = "Jaspreet"
+last_name = "Saini"
+_id = '6969afd9'
+name2 = "Jas69"
+
+
+#invalid identifiers
+2name = "Jas69"
+marks$ = 98
+
+
+print(age)
+print(name)
