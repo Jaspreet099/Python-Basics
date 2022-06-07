89147bae1199c69fd805a19534781ff1376870a6
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-06-07 22:48:00 +0530

    Conditional Statement

---
+++ 05 Conditional Statements/if_elif_else.py
@@ -0,0 +1,8 @@
+num = -3.4
+
+if num > 0:
+    print("positive number")
+elif num ==0:
+    print("Zero")
+else:
+    print("Negative Number")
---
+++ 05 Conditional Statements/if_else.py
@@ -0,0 +1,6 @@
+age  = 3
+
+if age > 18:
+    print('You can vote')
+else:
+    print('You cant vote')
