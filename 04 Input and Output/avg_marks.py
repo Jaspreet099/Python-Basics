6f3eff5088bb5de61a4ba8754265005de6ece7dc
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-06-07 22:33:35 +0530

    Input and output

---
+++ 04 Input and Output/avg_marks.py
@@ -0,0 +1,5 @@
+english_marks = int(input("Enter your English marks:"))
+science_marks = int(input("Enter your Science marks:"))
+maths_marks = int(input("Enter your Maths marks:"))
+
+print('Average of marks is:', (english_marks+science_marks+maths_marks)/3)
