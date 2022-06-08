0096ab01d21477b6f2be598b2b1e7fe5d2463940
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-06-08 22:52:01 +0530

    strings

---
+++ 08 Strings/Strings.py
@@ -0,0 +1,27 @@
+name = "Jaspreet"
+
+paragraph = ''' This is 
+a 
+Multiline
+String'''
+
+# print(paragraph)
+
+#access string char
+
+fruit = "apple"
+
+print(fruit[2])
+print(fruit[-1])
+
+# my_char = fruit[3] #assigning char
+
+a = "abc"
+b = "efg"
+
+# print(a*3) #Multiplying string
+# print(a+b) #sting concanate
+
+
+for my_char in a:
+    print(my_char*2)
---
+++ 08 Strings/string_methods.py
@@ -0,0 +1,14 @@
+# password = "abcdAB$"
+# print(password.isalpha())
+
+# s = "123"
+# print(s.isdigit())
+
+s = "abcde"
+print('s.isalpha', s.isalpha())
+print('s.isdigit', s.isdigit())
+print('s.islower', s.islower())
+print('s.isupper', s.isupper())
+
+print('s.lower', s.lower())
+print('s.upper', s.upper())
