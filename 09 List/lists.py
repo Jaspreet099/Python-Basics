9db0eb76a69be73ab097b101fc40f8f3529a1920
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-06-12 23:41:14 +0530

    Lists comprehension

--- 09 List/lists.py
+++ 09 List/lists.py
@@ -1,6 +1,17 @@
-my_list = [1,2,3, "Jaspreet"]
-print(my_list)
+
+# my_list = [1,2,3, "Jaspreet"]
+# print(my_list)
 
 
-fruits = ["apple", "guava", "papaya"]
-print(fruits[-2])
+# fruits = ["apple", "guava", "papaya"]
+# fruits[1] = "banana"
+
+# del fruits[0]
+# print(fruits)
+
+# list comprehension
+a = [x for x in range(100) if x%2 == 1]
+print(a)
+
+b = [3 ** i for i in range(10)]
+print(b)
